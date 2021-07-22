6ac87734fcd0e3d7d2f8767db4bb85c3aaa3adc4
Jason Luppnow - Snowstorm <18818835@student.curtin.edu.au>
2021-07-22 15:58:17 +0800

    Added fragment manager code to add initial fragments.

--- app/src/main/java/com/example/mangareader/MainActivity.java
+++ app/src/main/java/com/example/mangareader/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.mangareader;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+
+import com.example.mangareader.NavigationBar.HistoryItemUnselectedFragment;
+import com.example.mangareader.NavigationBar.LibraryItemSelectedFragment;
+import com.example.mangareader.NavigationBar.MoreItemUnselectedFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Initialize fragments.
+        ListTypeSelectedDefaultFragment defaultListTypeFragment = new ListTypeSelectedDefaultFragment();
+        ListTypeUnselectedDownloadedFragment downloadedListTypeFragment = new ListTypeUnselectedDownloadedFragment();
+
+        //Fetch fragment manager.
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        if (savedInstanceState == null)
+        {
+            //Add Header Fragment.
+            fragmentManager.beginTransaction().add(R.id.defaultListTypeFragment, new HeaderFragment());
+
+            //Add Top List Types.
+            //Add Default List Type Fragment.
+            fragmentManager.beginTransaction().add(R.id.defaultListTypeFragment, defaultListTypeFragment, "DefaultListTypeSelected").commit();
+
+            //Add Downloaded List Type Fragment.
+            fragmentManager.beginTransaction().add(R.id.downloadedListTypeFragment, downloadedListTypeFragment, "DownloadedListTypeUnselected").commit();
+
+            //Add Recycler Fragment.
+
+            //Add Bottom Navigation Item Fragments.
+            //Add Library Navigation Item Fragment.
+            fragmentManager.beginTransaction().add(R.id.libraryNavigationItem, new LibraryItemSelectedFragment(), "LibraryNavigationItemSelected").commit();
+
+            //Add History Navigation Item Fragment.
+            fragmentManager.beginTransaction().add(R.id.historyNavigationItem, new HistoryItemUnselectedFragment(), "HistoryNavigationItemSelected").commit();
+
+            //Add More Navigation Item Fragment.
+            fragmentManager.beginTransaction().add(R.id.moreNavigationItem, new MoreItemUnselectedFragment(), "MoreNavigationItemSelected").commit();
+        }
     }
 }
