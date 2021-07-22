13a1cde7de9f407e58d36d47f8133bd622ad2ef8
Jason Luppnow - Snowstorm <18818835@student.curtin.edu.au>
2021-07-22 15:47:28 +0800

    Added fragments and resources for bottom navigation bar.

---
+++ app/src/main/java/com/example/mangareader/NavigationBar/HistoryItemSelectedFragment.java
@@ -0,0 +1,21 @@
+package com.example.mangareader.NavigationBar;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.mangareader.R;
+
+public class HistoryItemSelectedFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment.
+        return inflater.inflate(R.layout.fragment_history_item_selected, container, false);
+    }
+}
