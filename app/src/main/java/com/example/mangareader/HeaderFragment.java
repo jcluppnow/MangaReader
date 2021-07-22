0379f215cc96f4d0b6035cc72c791c7a005154a4
Jason Luppnow - Snowstorm <18818835@student.curtin.edu.au>
2021-07-22 14:59:21 +0800

    Added Header Navigation Fragment.

---
+++ app/src/main/java/com/example/mangareader/HeaderFragment.java
@@ -0,0 +1,22 @@
+package com.example.mangareader;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class HeaderFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view  = inflater.inflate(R.layout.fragment_header, container, false);
+
+        return view;
+    }
+}
