130b2142a269817193161129532c3970b3144ec7
jcluppnow <55492646+jcluppnow@users.noreply.github.com>
2021-07-22 22:21:49 +0800

    XML: Fixed more item xml & added library item xml that was missing.

--- app/src/main/java/com/example/mangareader/NavigationBar/HistoryItemUnselectedFragment.java
+++ app/src/main/java/com/example/mangareader/NavigationBar/HistoryItemUnselectedFragment.java
@@ -3,11 +3,14 @@ package com.example.mangareader.NavigationBar;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.mangareader.MainMenu.ListTypeSelectedDefaultFragment;
+import com.example.mangareader.MainMenu.ListTypeUnselectedDownloadedFragment;
 import com.example.mangareader.R;
 
 public class HistoryItemUnselectedFragment extends Fragment {
@@ -16,6 +19,30 @@ public class HistoryItemUnselectedFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_history_item_unselected, container, false);
+        View view = inflater.inflate(R.layout.fragment_history_item_unselected, container, false);
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectHistoryEventHandler();
+            }
+        });
+
+        return view;
+    }
+
+    private void selectHistoryEventHandler()
+    {
+        //Get Fragment Manager from Parent Activity.
+        FragmentManager fragmentManager = getParentFragmentManager();
+
+        //Mark the Library Item as unselected.
+        fragmentManager.beginTransaction().replace(R.id.libraryNavigationItem, new LibraryItemUnselectedFragment()).commit();
+
+        //Mark the History Item as selected.
+        fragmentManager.beginTransaction().replace(R.id.historyNavigationItem, new HistoryItemSelectedFragment()).commit();
+
+        //Mark the More Item as unselected.
+        fragmentManager.beginTransaction().replace(R.id.moreNavigationItem, new MoreItemUnselectedFragment()).commit();
     }
 }
