e1da3006adf8ec25a317cb5a57b04efe7dc314f0
jcluppnow <55492646+jcluppnow@users.noreply.github.com>
2021-07-22 23:27:08 +0800

    Add More Activity and Fragments for More Navigation Bar.

---
+++ app/src/main/java/com/example/mangareader/MorePage/MoreActivity.java
@@ -0,0 +1,16 @@
+package com.example.mangareader.MorePage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mangareader.R;
+
+public class MoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_more);
+    }
+}
